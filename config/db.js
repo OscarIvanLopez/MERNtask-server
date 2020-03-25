494a02a2b6c0722d4ea34c515f08b5a207e7f0b8
Ivan Lopez <oscari.lopez@potros.itson.edu.mx>
2020-03-25 14:37:16 -0700

    commit

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
-require('dotenv').config({path: 'variables.env'});
+require('dotenv').config({ path: 'variables.env' });
 
-const conectarDB = async () =>{
-    try{
+const conectarDB = async () => {
+    try {
         await mongoose.connect(process.env.DB_MONGO, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
         });
         console.log('DB Conectada');
-    }catch(error){
+    } catch (error) {
+        console.log('hubo un error')
         console.log(error);
-        process.exit(1); //!Detener app
+        process.exit(1); //! Detener la app
     }
 }
 
--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require('express');
-const conectarDB = require('./config/db')
+const conectarDB = require('./config/db');
+const cors = require('cors');
 
-//* Crear el servidor 
+// crear el servidor
 const app = express();
 
-//* Conectar base de datos  
+// Conectar a la base de datos
 conectarDB();
 
-//* Habilitar express.json
-app.use(express.json({extended: true}));
+// habilitar cors
+app.use(cors());
 
-//* Puerto de la app
-const PORT = process.env.PORT || 4000;
+// Habilitar express.json
+app.use( express.json({ extended: true }));
 
-//* Importar rutas
+// puerto de la app
+const port = process.env.PORT || 4000;
+
+// Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-
-
-//* Arrancar la app
-app.listen(PORT, () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
-})
+// arrancar la app
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
